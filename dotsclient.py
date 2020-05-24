10714f46cb89f7bf8c5bb68bb6ae5903a143806d
takayukikonishi <58019089+takayukikonishi@users.noreply.github.com>
2020-05-24 14:53:05 +0900

    Update dotsclient.py and dotsserver.py
    Supporting CBOR.
    Currently not supported.

--- dotsclient.py
+++ dotsclient.py
@@ -1,55 +1,38 @@
 import json
-import socket
 import sys
 
+import cbor2
 from coapthon.client.helperclient import HelperClient
 
 
+class DOTSClient():
+    SIGNALCHANNEL_PORT = 4646
+
+    def __init__(self, host):
+        self.host = host
+        self.path = None
+        self.payload = None
+        self.client = HelperClient(
+            server=(self.host, DOTSClient.SIGNALCHANNEL_PORT))
+
+    def PUT(self, path, payload):
+        self.path = path
+        self.payload = payload
+        try:
+            response = self.client.put(self.path, cbor2.dumps(self.payload))
+            print(response.pretty_print())
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.client.stop()
+
+
 def main():
-    # JSONメッセージを作成
-    client_id = int(sys.argv[1])
-    registration_request = {
-        "ietf-dots-data-channel:dots-client":
-        {
-            "client-id": client_id
-        }
-    }
-    data = json.dumps(registration_request)
-
-    # TCPで送信
-    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientsocket.connect(('127.0.0.1', int(sys.argv[3])))
-    clientsocket.send(data.encode())
-    clientsocket.close()
-
-    # 緩和要請のJSONファイル読み込み
-    with open(sys.argv[2], 'r') as f:
+    with open('Test.json', 'r') as f:
         mitigation_request = json.dumps(json.load(f))
 
-    # 緩和要請の送信許可
-    while True:
-        answer = input('緩和要請を送信しますか？（y/n）：　')
-        if answer == 'y':
-            break
-        elif answer == 'n':
-            sys.exit()
-        else:
-            pass
-
-    # CoAPのPUTメソッドで送信
-    host = '127.0.0.1'
-    port = int(sys.argv[4])
-    path = 'dotsserver'
-    payload = mitigation_request
-
-    client = HelperClient(server=(host, port))
-    try:
-        response = client.put(path, payload)
-        print(response.pretty_print())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        client.stop()
+    dots_client = DOTSClient(sys.argv[1])
+    dots_client.PUT('dotsserver', mitigation_request)
 
 
 if __name__ == '__main__':
