3aeafa7c1ef3b946848b02278276d8609053e4bb
Takayuki Konishi <takayuki.konishi.0804@gmail.com>
2020-06-29 05:44:26 +0000

    Update mitigation.py

--- localmanager.py
+++ localmanager.py
@@ -14,19 +14,26 @@ class LocalManager(Resource):
 
     def render_PUT(self, request):
         self.payload = request.payload
+        mitigation = Mitigation(self.router)
         for scope in json.loads(self.payload)['ietf-dots-signal-channel:mitigation-scope']['scope'] :
             for target_prefix in scope['target-prefix'] :
                 target_ip = re.sub('/\d*', '', target_prefix)
                 print(target_ip)
-                mitigation = Mitigation(target_ip, self.router)
-                mitigation.firewall()
-                if request.source[0] != '10.0.0.2':
-                    client = HelperClient(server=('10.0.0.2', 4646))
-                    try:
-                        response = client.put('globalmanager', self.payload)
-                        print(response.pretty_print())
-                    except KeyboardInterrupt:
-                        pass
-                    finally:
-                        client.stop()
+                for target_port_range in scope['target-port-range'] :
+                    target_lower_port = target_port_range['lower-port']
+                    for target_protocol in scope['target-protocol'] :
+                        if target_protocol == 6 :
+                            target_protocol = 'tcp'
+                        elif target_protocol == 17 :
+                            target_protocol = 'udp'
+                        mitigation.firewall(target_ip, target_lower_port, target_protocol)
+                        if request.source[0] != '10.1.0.2':
+                            client = HelperClient(server=('10.1.0.2', 4646))
+                        try:
+                            response = client.put('globalmanager', self.payload)
+                            print(response.pretty_print())
+                        except KeyboardInterrupt:
+                            pass
+                        finally:
+                            client.stop()
         return self
--- mitigation.py
+++ mitigation.py
@@ -1,16 +1,16 @@
 import paramiko
 
 class Mitigation():
-    def __init__(self, target_ip, router):
-        self.target_ip = target_ip
+    def __init__(self, router):
         self.router = router
 
-    def firewall(self):
+    def firewall(self, target_ip, target_lower_port, target_protocol):
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(self.router, username='tkonishi', password='0000')
+        print(f'sudo ufw route deny to {target_ip} port {target_lower_port} proto {target_protocol}')
 
-        stdin, stdout, stderr = client.exec_command(f'sudo ufw route deny to {self.target_ip}')
+        stdin, stdout, stderr = client.exec_command(f'sudo ufw route deny to {target_ip} port {target_lower_port} proto {target_protocol}')
         for line in stdout:
             print(line)
 
