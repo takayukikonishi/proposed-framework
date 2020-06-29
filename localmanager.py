56714bc03a2dc94e12cf41e12412fa1b7e1881b6
Takayuki Konishi <takayuki.konishi.0804@gmail.com>
2020-06-29 06:24:59 +0000

    Update localmanager.py

--- localmanager.py
+++ localmanager.py
@@ -29,11 +29,11 @@ class LocalManager(Resource):
                         mitigation.firewall(target_ip, target_lower_port, target_protocol)
                         if request.source[0] != '10.1.0.2':
                             client = HelperClient(server=('10.1.0.2', 4646))
-                        try:
-                            response = client.put('globalmanager', self.payload)
-                            print(response.pretty_print())
-                        except KeyboardInterrupt:
-                            pass
-                        finally:
-                            client.stop()
+                            try:
+                                response = client.put('globalmanager', self.payload)
+                                print(response.pretty_print())
+                            except KeyboardInterrupt:
+                                pass
+                            finally:
+                                client.stop()
         return self
